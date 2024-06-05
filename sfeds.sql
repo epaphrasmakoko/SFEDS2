a436dd6433e498770379b2268867c0c199a26e54
Epaphras Makoko <epaphrasmakoko55@gmail.com>
2024-06-05 10:14:51 +0300

    presentation

--- sfeds.sql
+++ sfeds.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 20, 2024 at 08:48 PM
+-- Generation Time: Jun 05, 2024 at 09:13 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -34,18 +34,19 @@ CREATE TABLE `users` (
   `phone_number` varchar(15) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `is_active` tinyint(1) DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `first_name`, `last_name`, `phone_number`, `email`, `password`, `created_at`) VALUES
-(1, 'Epaphras', 'Makoko', '0742029829', 'epaphrasmakoko55@gmail.com', '$2y$10$lLbiwNJfWhqtoyGqfBRqfuC3BIBsUISUyXsdKhsPGuCYZTWP6Olsu', '2024-05-20 08:24:59'),
-(2, 'jesca', 'kagande', '0747079196', 'jescakagande@gmail.com', '$2y$10$lPniAJSd4HBOagER7OP6Cel03ANEzZcza1GTt7H2s1OdCLYi3o/UO', '2024-05-20 09:08:04'),
-(3, 'collins', 'kimweri', '0756290460', 'collinskimweri@gmail.com', '$2y$10$qQPpoBJoLZlbBocTb0Kne.Cag4rwnw1sRT1a/45Xa87eoXHRevj6e', '2024-05-20 13:55:00'),
-(4, 'admin', 'admin', '1234567890', 'admin@gmail.com', '$2y$10$uyM/TVsbuu1ur7wXrKsh7eqYIvRVfwSTyRZ4Xd/b1IaIvu/by2upi', '2024-05-20 15:51:28');
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `phone_number`, `email`, `password`, `created_at`, `is_active`) VALUES
+(6, 'jesca', 'kagande', '255742029829', 'jescakagande@gmail.com', '$2y$10$.WYXrzlaxAqQZX7RnZJtGOAA6VqrybYI07hpKDUCbS5tzkGvYWxFO', '2024-05-22 19:14:26', 1),
+(12, 'mariel', 'fred', '255742029829', 'marielfred@gmail.com', '$2y$10$3Ok.r1OMDI.GjQS.fmFuEurdfUiV7P12PYTmToQwT0Z.2iwZF6Mla', '2024-05-22 21:14:06', 1),
+(13, 'admin', 'admin', '255742029829', 'admin@gmail.com', '$2y$10$LOYIV.poDy0TC8XnpijCKOIWdxThUL2/jlT1pJyxPI6q3YLyMM4ea', '2024-05-22 21:58:32', 1),
+(14, 'Epaphras', 'Makoko', '255742029829', 'epaphrasmakoko55@gmail.com', '$2y$10$5O6ZcPfj7JMFoM5nJFtqT.4Pekr8RzUK6y5WVPsDwrQzoZQVX.E0W', '2024-05-22 22:22:59', 1);
 
 --
 -- Indexes for dumped tables
@@ -66,7 +67,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
